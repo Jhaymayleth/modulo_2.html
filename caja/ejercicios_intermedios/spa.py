5c8968fc3319aee0ef029f387fc65ec6c5659ff3
jose rangel <josemrn2006@gmail.com>
2026-04-11 21:45:25 -0500

    eliminando carpetas

--- caja/ejercicios_intermedios/spa.py
+++
@@ -1,9 +0,0 @@
-#9. Spa: servicio
-
-servicio = input("Servicio (masaje, facial, manicure): ").lower()
-
-if servicio == "masaje" or servicio == "facial" or servicio == "manicure":
-    print("Servicio disponible")
-else:
-    print("Servicio no disponible")
-
